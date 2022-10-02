d8ef095be427db48a97f4505845f8ebb9fb2f26d
jb <jb@debugger2.net.loco>
2022-10-02 17:41:47 +0530

    monad tests

--- monad.lua
+++ monad.lua
@@ -1,36 +1,74 @@
+local MObj = {value=nil, Type=nil}
+function MObj:eq(mobj)
+	return self.value == mobj.value
+end
+function MObj.of(value, typeT)
+	local o = {value=normal}
+	setmetatable(o, {__index = typeT})
+	return o
+end
+
 local Maybe   = {}
-local Just    = {Type = "Just", s=nil}
+setmetatable(Maybe, {__index = MObj})
+
+local Just    = {Type = "Just"}
+setmetatable(Just, {__index = Maybe})
+
 local Nothing = {Type = "Nothing"}
-function Just:of(s)
+setmetatable(Nothing, {__index = Maybe})
+
+-- of :: a -> Maybe a
+function Just.of(s)
 	if s == nil then
 		return Nothing
 	end
-	local o = {s=s}
+	local o = {value=s}
 	setmetatable(o, {__index = Just})
 	return o
 end
+
+-- fmap :: (a -> b) -> Maybe a -> Maybe b
 function Maybe:fmap(f)
 	if self.Type == "Just" then
-		return Just:of(f(self.s))
+		return Just.of(f(self.value))
 	else
 		return Nothing
 	end
 end
-setmetatable(Just, {__index = Maybe})
-setmetatable(Nothing, {__index = Maybe})
+
+-- bind:: (a -> Maybe b) -> Maybe a -> Maybe b
+function Maybe:bind(f)
+	return f(self.value)
+end
+
 local List = {Type="List"}
-function List:of(t)
+setmetatable(List, {__index = MObj})
+
+-- of :: a -> List a
+function List.of(t)
 	local o = t or {}
 	setmetatable(o, {__index = List})
 	return o
 end
+
+-- fmap :: (a -> b) -> List a -> List b
 function List:fmap(f)
 	local o = {}
 	for k,v in pairs(self) do
 		o[k] = f(v)
 	end
-	return List:of(o)
+	return List.of(o)
 end
+
+-- bind:: (a -> List b) -> List a -> List b
+function List:bind(f)
+	local o = {}
+	for k,v in pairs(self) do
+		o[k] = f(v)[1]
+	end
+	return List.of(o)
+end
+
 return 
 	{ Maybe=Maybe
 	, Just=Just
--- tests/monad.lua
+++ tests/monad.lua
@@ -9,44 +9,35 @@ local f = function(x) return x*x end
 local g = function(x) return x+1 end
 
 function test_just_fmap()
-	local m = M.Just
-		:of(12)
-		:fmap(f)
-	assert(m.Type == "Just")	
-	assert(m.s == 144)	
+	local m = M.Just.of(12):fmap(f)
+	assert(m:eq(M.Just.of(144)))	
 end
 
 function test_just_fmap_nil()
-	local m = M.Just
-		:of(123)
+	local m = M.Just.of(123)
 		:fmap(function(x) return nil end)
-	assert(m.Type == "Nothing")	
-	assert(m.s == nil)	
+	assert(m:eq(M.Nothing))	
 end
 
 function test_just_fmap_nothing()
 	local m = M.Nothing
 		:fmap(function(x) return nil end)
-	assert(m.Type == "Nothing")	
-	assert(m.s == nil)	
+	assert(m:eq(M.Nothing))	
 end
 
 function test_just_id()
-	local m = M.Just:of(8)
-	assert(m.s == 8)
+	local m = M.Just.of(8)
+	assert(m.value == 8)
 end
 
 function test_just_assoc()
-	local m = M.Just:of(8)
-		:fmap(f)
-		:fmap(g)
-	local r = M.Just:of(f(8))
-		:fmap(g)
-	assert(m.s == r.s)
+	local m = M.Just.of(8):fmap(f):fmap(g)
+	local r = M.Just.of(f(8)):fmap(g)
+	assert(m:eq(r))
 end
 
 function test_list_fmap()
-	local m = M.List:of({1,2,3,4})
+	local m = M.List.of({1,2,3,4})
 		:fmap(f)
 		:fmap(g)
 	assert(m.Type == "List")
@@ -54,11 +45,40 @@ function test_list_fmap()
 end
 
 function test_dict_fmap()
-	local m = M.List:of({a=1,b=2,c=3,d=4})
+	local m = M.List.of({a=1,b=2,c=3,d=4})
 		:fmap(f)
 		:fmap(g)
 	assert(m.Type == "List")
 	assert(m.c == 10)
+end
+
+-- monadic laws
+-- M a >>= f  => M f(a)
+-- M a >>= of => M a
+-- M a >>= f >>= g => M a >>= f(g)
+
+local mf = function(x) return M.Just.of(x*x) end
+local mg = function(x) return M.Just.of(x+1) end
+
+function test_just_monad_law_1()
+	local m = M.Just.of(8):bind(mf)
+	local r = mf(8)
+	assert(m:eq(r))
+end
+
+function test_just_monad_law_2()
+	local m = M.Just.of(8):bind(M.Just.of)
+	local r = M.Just.of(8)
+	assert(m:eq(r))
+end
+
+function test_just_monad_law_3()
+	local m = M.Just.of(8):bind(mf):bind(mg)
+	local r = M.Just.of(8):bind(function(x)
+		local y = mf(x)
+		return  mg(y.value)
+	end)
+	assert(m:eq(r))
 end
 
 os.exit( luaunit.LuaUnit.run() )
